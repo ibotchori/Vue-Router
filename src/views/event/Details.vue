ac68d1e8bfe183adcbd6e9388c701ba5c7e0c92b
koregeo <koregm@gmail.com>
2021-02-17 13:55:17 +0400

    Add Layout component & edit child components (Nested Routes)

--- src/views/event/Details.vue
+++ src/views/event/Details.vue
@@ -1,44 +1,9 @@
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <div id="nav">
-      <!-- URL= /event/2 -->
-      <router-link :to="{ name: 'EventDetails', params: { id } }"
-        >Details</router-link
-      >
-      |
-      <!-- URL= /event/2/register -->
-      <router-link :to="{ name: 'EventRegister', params: { id } }"
-        >Register</router-link
-      >
-      |
-      <!-- URL= /event/2/edit  -->
-      <router-link :to="{ name: 'EventEdit', params: { id } }"
-        >Edit</router-link
-      >
-    </div>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
-  </div>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
 </template>
-
 <script>
-import EventService from '@/services/EventService.js'
 export default {
-  props: ['id'], // <-- id prop
-  data() {
-    return {
-      event: null // <--event object which update temlpate
-    }
-  },
-  created() {
-    EventService.getEvent(this.id) // <--- Use id prop to get information from API
-      .then(response => {
-        this.event = response.data // <-- Event data which stored to event object
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }
+  props: ['event'] // <<-- Receives event from Layout
 }
 </script>
