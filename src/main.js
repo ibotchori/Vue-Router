943469dcd1fe8da4e1d3b0f0db5888c46772fa53
koregeo <koregm@gmail.com>
2021-02-24 18:10:08 +0400

    Global Storage (Flash Messages)

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from 'vue'
+import { createApp, reactive } from 'vue' // <--- importing reactive object
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Reactive object, we use this as a Global Storage
+const GStore = reactive({ flashMessage: '' })
+
 createApp(App)
   .use(store)
+  .provide('GStore', GStore)  // <--- Provide this Reactive object ('key', value) so others (components) can use (inject) it
   .use(router)
   .mount('#app')
