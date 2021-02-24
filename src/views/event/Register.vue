aa0d8831537335ece6965fbc22790e31a43f961a
koregeo <koregm@gmail.com>
2021-02-24 18:22:30 +0400

    Create the Flash Message (Flash Messages)

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,16 +1,25 @@
 <template>
-   <p>Register for the event here</p>
-  <button @click="register">Register Me!</button>  <!-- run register method on button click -->
+  <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
+  <!-- run register method on button click -->
 </template>
 <script>
 export default {
   props: ['event'], // <<-- Receives event from Layout
+  inject: ['GStore'], // <---- Inject the Global Store, make the GStore object available inside this component
   methods: {
     register() {
-      // If registration API call is successful
-      // Push back to the event details
+      // Assuming successful API call to register them
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title // <-- Flash message
+      setTimeout(() => {
+        // After 3 seconds remove flash message
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      // If registration API call is successful, push back to the event details
       this.$router.push({
-        name: 'EventDetails',
+        name: 'EventDetails'
         //params: { id: this.event.id } <-- we use object'id to navigate same object which router-link uses
         // But we don't need to specify the ID, it'll get populated by default
       })
