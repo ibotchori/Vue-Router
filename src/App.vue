293f5d47dc951588279f650801116fe30ab3bd2c
koregeo <koregm@gmail.com>
2021-02-24 18:45:01 +0400

    Displaying the Flash Message (Flash Messages)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="flashMessage" v-if="GStore.flashMessage">
+      <!-- displaying the flashMessage if it exists -->
+      {{ GStore.flashMessage /* Flash Message */ }}
+    </div>
     <div id="nav">
       <router-link :to="{ name: 'EventList' }">Events</router-link> |
       <router-link :to="{ name: 'About' }">About</router-link>
@@ -8,7 +12,28 @@
   </div>
 </template>
 
+<script>
+export default {
+  inject: ['GStore'] // <---- Inject GStore object for displaying flashMessage
+}
+</script>
+
 <style>
+/* Flash Message - below this comment*/
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
+/* Flash Message - above this comment */
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
